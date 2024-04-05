1adb3074b48dd0bfe0252871f26f1fa70e87d3a6
Zukay Kiryl <kzykov03@mail.ru>
2024-04-06 01:45:22 +0300

    add GetPage and AddPage

--- app.js
+++ app.js
@@ -14,13 +14,18 @@ app.set('view engine', 'hbs');
 
 app.set('views', 'views'); // default views
 
-app.use(express.static('style'));
+app.use('/style', express.static('style'));
+app.use('/scripts', express.static('scripts'));
 
 
-const persons = [
+let persons = [
+    { name: "Стадольник Алексей Владимирович", phone: "+375291843130" },
+    { name: "фывафыва", phone: 10 },
+    { name: "фывафыва", phone: 40 },
     { name: "Nils", phone: 20 },
     { name: "Teddy", phone: 10 },
     { name: "Nelson", phone: 40 },
+    { name: "Nils", phone: 20 },
   ]
 
 
@@ -31,14 +36,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/Add', (req, res) => {
-    res.render('AddForm');
+    res.render('AddForm', {persons});
 });
 
 app.get('/Update', (req, res) => {
     res.render('UpdateForm');
 });
 
+app.post('/Add', (req, res) => {
+    console.log(1111111)
+    const newItem = req.body;
+    
+    console.log(newItem);
 
+    res.status(200).send(newItem)
+})
 app.listen(3000, () => {
     console.log("Server started on port 3000")
 });
